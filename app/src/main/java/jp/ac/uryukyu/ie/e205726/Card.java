b98a86d61d61f2467cf7aec7b3d29d7044edbf9a
Hiroki Teruya <e205726@ie.u-ryukyu.ac.jp>
2023-02-08 19:40:23 +0900

    commit4:山札作成_修正

--- app/src/main/java/jp/ac/uryukyu/ie/e205726/Card.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205726/Card.java
@@ -1,18 +1,31 @@
 package jp.ac.uryukyu.ie.e205726;
 import java.util.Collection;
+
+import javax.management.AttributeChangeNotificationFilter;
+
 import java.util.ArrayList;
 
 public class Card {
-    public String[] mark = {"Sp", "He", "Cl", "Di"};
-    public String[] number = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
-    ArrayList<String> Deck = new ArrayList<>();
+    private String[] mark = {"Sp", "He", "Cl", "Di","baba"};
+    private String[] number = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+    ArrayList<ArrayList<String>> Deck = new ArrayList<ArrayList<String>>();
 
-    void Cards(){
-        for (int i = 0; i<4; i++){
+    public void Cards(){
+        for (int i = 0; i<5; i++){
             for (int j = 0; j<13; j++){
-                Deck.add(mark[i]+number[j]);
+                ArrayList card = new ArrayList<>();
+                if (mark[i]== "baba"){
+                    card.add(mark[i]);
+                    card.add("baba");
+                    Deck.add(card);
+                    break;
+                }
+                card.add(mark[i]);
+                card.add(number[j]);
+                Deck.add(card);
             }
         }
         System.out.println(Deck);
+
     }
 }
